7e279f20021fb9b45184aed442f49414f947d407
Alejandro Melendez <amelendezto@gmail.com>
2023-08-15 11:29:15 -0600

    Changes to Car Agent

--- Scripts/agents.py
+++ Scripts/agents.py
@@ -17,19 +17,15 @@ import time
 import datetime
 import random
 
-
 def get_grid(model):
-    '''
-    descripcion...
-    '''
     grid = np.zeros((model.grid.width, model.grid.height))
     for cell in model.grid.coord_iter():
       cell_content, (x, y) = cell
       for obj in cell_content:
         if isinstance(obj, Car):
-          grid[x][y] = 0.4
+          grid[x][y] = 4
         elif isinstance(obj, TrafficLight):
-          grid[x][y] = 0.1 + obj.state * .1
+          grid[x][y] = obj.state + 1
     return grid
 
 class Car(Agent):
@@ -43,39 +39,38 @@ class Car(Agent):
         self.traffic_light = tl
         self.trigger = False
     
+    def move(self):
+        cellmates = self.model.grid.get_cell_list_contents((self.pos[0] + self.dir[0], self.pos[1] + self.dir[1]))
+        if len(cellmates) == 0:
+            self.next_pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+
     def step(self):
-
-        if (not self.trigger):
-            if (self.traffic_light.state == 2):
-                self.next_pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
-            
-            if (self.next_pos[0] == self.traffic_light.pos[0] or self.next_pos[1] == self.traffic_light.pos[1]):
-                self.trigger = True
-        else:
-            # if we have arrived at the end of the grid remove the agent
-            if (self.pos == self.dest):
-                #self.model.grid.remove_agent(self)
-                return
+        if   ((self.pos[0] == self.traffic_light.pos[0] or self.pos[0] == self.traffic_light.pos[0]+1)and(self.pos[1] < self.traffic_light.pos[1]-1) and self.traffic_light.unique_id == 1+50):
+            if self.traffic_light.state == 2:
+                pass
             else:
-                if (self.pos[0] == self.dest[0]):
-                    if (self.pos[1] < self.dest[1]):
-                        self.dir = (0,1)
-                    else:
-                        self.dir = (0,-1)
-                else:
-                    if (self.pos[0] < self.dest[0]):
-                        self.dir = (1,0)
-                    else:
-                        self.dir = (-1,0)
-
-                self.next_pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+                self.move()
+        elif ((self.pos[0] < self.traffic_light.pos[0]-1)and(self.pos[1] == self.traffic_light.pos[1] or self.pos[1] == self.traffic_light.pos[1]-1) and self.traffic_light.unique_id == 2+50):
+            if self.traffic_light.state == 2:
+                pass
+            else:
+                self.move()
+        elif ((self.pos[0] == self.traffic_light.pos[0] or self.pos[0] == self.traffic_light.pos[0]-1)and(self.pos[1] > self.traffic_light.pos[1]+1) and self.traffic_light.unique_id == 3+50):
+            if self.traffic_light.state == 2:
+                pass
+            else:
+                self.move()
+        elif ((self.pos[0] > self.traffic_light.pos[0]+1)and(self.pos[1] == self.traffic_light.pos[1] or self.pos[1] == self.traffic_light.pos[1]+1) and self.traffic_light.unique_id == 4+50):
+            if self.traffic_light.state == 2:
+                pass
+            else:
+                self.move()
+        
         
     
     def advance(self):
-        #self.pos = self.next_pos
         self.model.grid.move_agent(self, self.next_pos)
             
-
 class TrafficLight(Agent):
     def __init__(self, unique_id, model, pos, state):
         # state: 0 = red, 1 = yellow, 2 = green
@@ -98,21 +93,27 @@ class Cross(Model):
         self.schedule = SimultaneousActivation(self)
         self.running = True
         self.trafic_lights = tl
-        self.yoyo = []
+        self.lights = []
+        self.contador = 0
 
         contador = 1
 
         for test in tl:
-
-            t = TrafficLight(contador, self, test, 2)
+            t = TrafficLight(contador+50, self, test, 0)
             contador += 1
             self.grid.place_agent(t, (test[0], test[1]))
             self.schedule.add(t)
-            self.yoyo.append(t)
+            self.lights.append(t)
 
-        car = Car(0, self, (12,0), (12,23), (0,1), self.yoyo[2])
-        self.grid.place_agent(car, (12,4))
-        self.schedule.add(car)
+        u = [(12,0) ,(13,0) ,(0,10),(0,11) ,(10,23),(11,23),(23,12),(23,13)]
+        d = [(12,23),(23,10),(10,0),(23,11),(0,13) ,(11,0) ,(0,12), (13,23)]
+        m = [(0,1),(0,1),(1,0),(1,0),(0,-1),(0,-1),(-1,0),(-1,0)]
+        o = [0,0,1,1,2,2,3,3]
+
+        for k in range(len(u)):
+            car = Car(k, self, u[k], d[k], m[k], self.lights[o[k]])
+            self.grid.place_agent(car, u[k])
+            self.schedule.add(car)
         
         self.datacollector = DataCollector(
                 model_reporters={"Grid": get_grid},  # A function to call
@@ -122,15 +123,26 @@ class Cross(Model):
     def step(self):
         self.schedule.step()
         self.datacollector.collect(self)
+        self.contador += 1
+        if contador % 10 == 0:
+            u = [(12,0) ,(13,0) ,(0,10),(0,11) ,(10,23),(11,23),(23,12),(23,13)]
+            d = [(12,23),(23,10),(10,0),(23,11),(0,13) ,(11,0) ,(0,12), (13,23)]
+            m = [(0,1),(0,1),(1,0),(1,0),(0,-1),(0,-1),(-1,0),(-1,0)]
+            o = [0,0,1,1,2,2,3,3]
 
-
+            for k in range(len(u)):
+                car = Car(100+k+self.contador, self, u[k], d[k], m[k], self.lights[o[k]])
+                self.grid.place_agent(car, u[k])
+                self.schedule.add(car)
     
 if __name__ == "__main__":
-    widht =23
-    height = 23  
+    widht = 24
+    height = 24  
     tiempo = 0.5
 
-    tl = [(10,11),(11,13),(12,10),(13,12)]
+    cmp = matplotlib.colors.ListedColormap(['white','red', 'yellow', 'green', 'black',])
+
+    tl = [(12,10),(10,11),(11,13),(13,12)]
     model = Cross(widht, height, tl)
     
     contador = 0
@@ -143,7 +155,10 @@ if __name__ == "__main__":
     fig, axs = plt.subplots(figsize=(7,7))
     axs.set_xticks([])
     axs.set_yticks([])
-    patch = plt.imshow(all_grid.iloc[0][0], cmap=plt.cm.binary)
+    # show the grid with color
+    patch = plt.imshow(all_grid.iloc[0][0], cmap=cmp)
+
+    #patch = plt.imshow(all_grid.iloc[0][0], cmap=plt.cm.binary)
 
     def animate(i):
         patch.set_data(all_grid.iloc[i][0])
@@ -154,4 +169,3 @@ if __name__ == "__main__":
 
     writergif = animation.PillowWriter(fps=10)
     anim.save('animation.gif', writer=writergif)
-
